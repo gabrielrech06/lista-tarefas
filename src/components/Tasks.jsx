1c69caac0ebdb5ad4598b06f1656577bcbab1ba0
Gabriel <ggralowrech@gmail.com>
2025-02-15 16:15:31 -0300

    Adicionando botão de detalhes

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,15 @@
+import { ChevronRightIcon } from "lucide-react";
+
 const Tasks = (props) => {
   return (
-    <ul className="space-y-4">
+    <ul className="space-y-4 p-6 bg-neutral-400 rounded-md shadow">
       {props.tasks.map((task) => (
-        <li key={task.id} className="bg-neutral-400 text-white p-2 rounded-md"> {task.title}</li>
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-neutral-500 w-full text-white p-2 rounded-md">{task.title}</button>
+          <button className="bg-neutral-500 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+        </li>
       ))}
     </ul>
   );
