880ba469e34266ffeeea5ce74c9619df6083b6e2
Gabriel <ggralowrech@gmail.com>
2025-02-15 16:35:04 -0300

    Adicionando função de tarefa completa

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,17 @@ const App = () => {
     },
   ]);
 
+  const onTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
   return (
     <div className="w-screen h-screen bg-neutral-900 flex justify-center p-6">
       <div className="w-[500px]">
@@ -32,7 +43,7 @@ const App = () => {
           Lista de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,14 @@ const Tasks = (props) => {
     <ul className="space-y-4 p-6 bg-neutral-400 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-neutral-500 w-full text-left text-white p-2 rounded-md">{task.title}</button>
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-neutral-500 w-full text-left text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
           <button className="bg-neutral-500 p-2 rounded-md text-white">
             <ChevronRightIcon />
           </button>
